af2189920904e017576f03bacd8b5a22dd745468
Anuz Pandey <anuzbvbmaniac123@gmail.com>
2023-01-12 22:57:07 +0545

    :hammer: refac: Category Seeder Issue Fix.

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,45 +17,50 @@ class CategorySeeder extends Seeder
     public function run()
     {
         Category::create([
-            'name' => 'Laravel',
+            'title' => 'Laravel',
             'slug' => 'laravel',
-            'description' => 'Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.',
+            'content' => 'Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.',
             'status' => TRUE,
         ]);
 
         Category::create([
-            'name' => 'PHP',
+            'title' => 'PHP',
             'slug' => 'php',
-            'description' => 'PHP is a popular general-purpose scripting language that is especially suited to web development.',
+            'content' => 'PHP is a popular general-purpose scripting language that is especially suited to web development.',
             'status' => TRUE,
         ]);
 
 
         // ---------------  OR  --------------- //
         // create multiple with insert method
+        // But with Insert uuid (id) cannot generate on its own so manually assigning id value
         Category::insert([
             [
-                'name' => 'JavaScript',
+                'id' => Str::uuid(),
+                'title' => 'JavaScript',
                 'slug' => 'javascript',
-                'description' => 'JavaScript is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions.',
+                'content' => 'JavaScript is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions.',
                 'status' => TRUE,
             ],
             [
-                'name' => 'Python',
+                'id' => Str::uuid(),
+                'title' => 'Python',
                 'slug' => 'python',
-                'description' => 'Python is an interpreted, high-level and general-purpose programming language.',
+                'content' => 'Python is an interpreted, high-level and general-purpose programming language.',
                 'status' => TRUE,
             ],
             [
-                'name' => 'Java',
+                'id' => Str::uuid(),
+                'title' => 'Java',
                 'slug' => 'java',
-                'description' => 'Java is a general-purpose programming language that is class-based, object-oriented, and designed to have as few implementation dependencies as possible.',
+                'content' => 'Java is a general-purpose programming language that is class-based, object-oriented, and designed to have as few implementation dependencies as possible.',
                 'status' => TRUE,
             ],
             [
-                'name' => 'C++',
+                'id' => Str::uuid(),
+                'title' => 'C++',
                 'slug' => 'c++',
-                'description' => 'C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes".',
+                'content' => 'C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes".',
                 'status' => TRUE,
             ]
         ]);
