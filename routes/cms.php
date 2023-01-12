20bb6a52348c3ae7b81df56d2dc956d41b14eef9
Anuz Pandey <anuzbvbmaniac123@gmail.com>
2023-01-12 22:11:19 +0545

    :zap: feat: Route for Category Model Created.

--- routes/cms.php
+++ routes/cms.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CMS\CategoryController;
 
 Route::group(['prefix' => 'cms', 'as' => 'cms.'], function () {
+
+    // Categories routes [Route url is plural and name is singular]
+    Route::get('categories', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('categories/create', [CategoryController::class, 'create'])->name('category.create');
+    Route::post('categories', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('categories/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::put('categories/{id}', [CategoryController::class, 'update'])->name('category.update');
 
 });
