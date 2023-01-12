bd92330872d25cdbf2d042f91eb97af38e131307
Anuz Pandey <anuzbvbmaniac123@gmail.com>
2023-01-12 22:18:50 +0545

    :construction: Caategory Index Method.

--- app/Http/Controllers/CMS/CategoryController.php
+++ app/Http/Controllers/CMS/CategoryController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\CMS;
 
+use App\Models\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        // Fetch all categories from database
+        $categories = Category::all();
+
+        return view('cms.categories.index', [
+            'categories' => $categories
+        ]);
+
+        // ------------- This return statement can be: (There are many ways to return data to view) Popular ones are:
+        // 1. return view('cms.categories.index', compact('categories'));
+        // 2. return view('cms.categories.index')->with('categories', $categories);
+    }
 }
